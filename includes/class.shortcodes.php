279071a8f3671f876904fbac6ec7b2deedc08b92
Beda Schmid <hello@tukutoi.com>
2020-11-22 14:28:16 +0700

    Added get term by shortcode

--- includes/class.shortcodes.php
+++ includes/class.shortcodes.php
@@ -37,6 +37,33 @@ class TWS_Shortcodes{
 		return '';
 
 	}
+	
+	/**
+	 * Get Term by
+	 * @return Term Object 
+	 */
+	function tws_get_term_by( $atts, $content = null ) {
+
+		$a = shortcode_atts( array(
+			'field' 	=> 'term_id',//'slug', 'name', 'id' or 'ID' (term_id), or 'term_taxonomy_id'.
+			'value' 	=> '1',//Search for this term value.
+			'taxonomy'	=> 'category',//Taxonomy name. Optional, if $field is 'term_taxonomy_id'.
+			'output'	=> 'name',//NOT Native - 'term_id', 'name', 'slug', 'term_group', 'term_taxonomy_id', 'taxonomy', 'description', 'parent', 'count', 'filter'
+			'filter'	=> 'raw'//'edit', 'db', 'display', 'attribute', or 'js'.
+		), $atts );
+
+		$term 	= get_term_by($a['field'], $a['value'], $a['taxonomy'], OBJECT, $a['filter']);
+
+		if(is_object($term) && !empty($term)){
+			$prop = $a['output'];
+			if( $a['output'] == 'description' && $a['filter'] == 'display' ){
+				return tws_return_shortcode($term->$prop);
+			}
+			return $term->$prop;
+		}
+		return '';
+
+	}
 
 	function tws_return_shortcode($content){
 		return wpv_do_shortcode($content);
