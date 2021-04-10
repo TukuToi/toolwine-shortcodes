e734c84336022e5550bca3e7d15241ee4977f966
Beda Schmid <hello@tukutoi.com>
2021-04-10 14:34:08 +0700

    Sync with other TukuToi Plugins #1

--- toolwine-shortcodes.php
+++ toolwine-shortcodes.php
@@ -10,7 +10,7 @@
  * Author URI:        https://www.tukutoi.com
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       toolwine-shortcodes
+ * Text Domain:       tw_shortcodes
  * Domain Path:       /languages
  */
 
