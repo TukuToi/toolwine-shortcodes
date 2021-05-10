331cbaf2a51f0208b073a4cdcc7bb2ba836cf039
Beda Schmid <hello@tukutoi.com>
2021-05-10 17:53:39 +0700

    Register tws_rep_field ShortCode

--- toolwine-shortcodes.php
+++ toolwine-shortcodes.php
@@ -73,6 +73,7 @@ function tws_init(){
       'tws_info'        => 'tws_shortcodes_info',
       'tws_current_url'	=> 'tws_current_url',
       'tws_round'	      => 'tws_round',
+      'tws_rep_fiel'    => 'tws_repeating_field',
     );
     $shortcodes     = apply_filters( 'tws_shortcodes', $shortcodes );
     $tws_shortcodes = new TWS_Shortcodes( $shortcodes );
